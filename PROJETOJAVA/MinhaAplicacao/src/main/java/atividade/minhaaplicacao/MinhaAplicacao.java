db63add04919119d07ecf88a17dbb512cd363505
Sara Coni <conisara96@gmail.com>
2026-09-09 11:50:51 -0300

    Atividade Java

---
+++ PROJETOJAVA/MinhaAplicacao/src/main/java/atividade/minhaaplicacao/MinhaAplicacao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package atividade.minhaaplicacao;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class MinhaAplicacao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ PROJETOJAVA/MinhaAplicacao/src/main/java/modelo/Uusuario.java
@@ -0,0 +1,47 @@
+package modelo; 
+public class Usuario { 
+private Long id; 
+private String nome; 
+private String cpf; 
+private String email; 
+private String telefone; 
+public String getCpf() { 
+return cpf; 
+} 
+public void setCpf(String cpf) { 
+Aula DS 28/05/2025 Professor Thiago/Sueli 
+        this.cpf = cpf; 
+    } 
+ 
+    public String getEmail() { 
+        return email; 
+    } 
+ 
+    public void setEmail(String email) { 
+        this.email = email; 
+    } 
+ 
+    public Long getId() { 
+        return id; 
+    } 
+ 
+    public void setId(Long id) { 
+        this.id = id; 
+    } 
+ 
+    public String getNome() { 
+        return nome; 
+    } 
+ 
+    public void setNome(String nome) { 
+        this.nome = nome; 
+    } 
+ 
+    public String getTelefone() { 
+        return telefone; 
+    } 
+ 
+    public void setTelefone(String telefone) { 
+        this.telefone = telefone; 
+    } 
+} 
